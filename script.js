fab7af7618ee7d30594a2e1519c97d790008090c
Elizeu <elizeu.schoroeder@gmail.com>
2019-11-08 23:07:51 -0300

    corrigido problema no código

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function addBola(){
   bola.setAttribute("class", "bola");
   var p1 = Math.floor(Math.random() * 500);
   var p2 = Math.floor(Math.random() * 400);
-  bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+  var cor1 = Math.floor(Math.random() * 256) - 1
+  var cor2 = Math.floor(Math.random() * 256) - 1
+  var cor3 = Math.floor(Math.random() * 256) - 1
+  bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-Color:rgb("+cor1+","+cor2+","+cor3+")");
+  document.body.setAttribute("style", "background-color: rgb("+cor1+","+cor2+","+cor3+");");
   bola.setAttribute("onclick", "estourar(this)");
-  var cores = cor.sort(randOrd)
-  bola.style.backgroundColor = cores[0];
 
   document.body.appendChild(bola);
 }
@@ -19,20 +21,6 @@ function iniciar() {
   setInterval(addBola, 1000);
 }
 
-function randOrd() {
-  return (Math.round(Math.random()) - 0.5);
-}
-var cor = Array();
-cor[0] = "#F88383";
-cor[1] = "#060606";
-cor[2] = "#65FA61";
-cor[3] = "#8A2BE2";
-cor[4] = "#FAAA49";
 
 
-setInterval(function () {
-  var cor1 = Math.floor(Math.random() * 256) - 1
-  var cor2 = Math.floor(Math.random() * 256) - 1
-  var cor3 = Math.floor(Math.random() * 256) - 1
-  document.body.setAttribute("style", "background-color: rgb("+cor1+","+cor2+","+cor3+");");
-}, 2000);
+
